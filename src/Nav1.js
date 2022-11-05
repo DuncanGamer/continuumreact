4a758fd0fb8d645a418d45996dea28fe1b1eed3e
Daniel G <dgimenez123@gmail.com>
2022-11-05 19:06:31 +0100

    Principio del proyecto

---
+++ src/Nav1.js
@@ -0,0 +1,27 @@
+
+function Nav1 (){
+    return(
+        <>
+         <div class="  hidden lg:block bg-slate-800 text-gray-300 py-2 text-sm">
+        <div class=" container  mx-auto flex justify-between">
+            <div class="flex items-center">
+                <div>
+                    Ne donnez pas suite aux demandes de prise de contact en dehors de la plateforme laissées dans des
+                    commentaires
+                    <i class="fa-solid fa-arrow-right"></i>
+                </div>
+            </div>
+            <div class="flex gap-x-4 items-center">
+                <a href="" class=" bg-do-blue-mediumdark px-3 py-1 rounded text-sky-400 font-semibold">We're Hiring</a>
+                <a href="" class=" hover:text-white">Blog</a>
+                <a href="" class=" hover:text-white">Info</a>
+                <a href="" class=" hover:text-white">Get Support</a>
+                <a href="" class=" hover:text-white">Sales</a>
+            </div>
+        </div>
+    </div>
+        </>
+    )
+}
+
+export default Nav1
---
+++ src/NavDesktop.js
@@ -0,0 +1,28 @@
+function NavDesktop (){
+    return (
+        <>
+         <div class="  hidden lg:block mt-6 shadow-md">
+        <div class=" container flex justify-between items-center mx-auto py-5">
+            <div class=" flex items-center gap-x-6 ">
+                <img src="/img/logo1.png" alt="logo continum" width="60px"></img>
+                <a href="#" class=" hover:text-gray-600">Products</a>
+                <a href="#" class=" hover:text-gray-600">Solutions</a>
+                <a href="#" class=" hover:text-gray-600">Marketplace</a>
+                <a href="#" class=" hover:text-gray-600">Community</a>
+                <a href="#" class=" hover:text-gray-600">Pricing</a>
+            </div>
+            <div class=" flex gap-4">
+                <a href="#"
+                    class=" hover:shadow-lg shadow-md shadow-gray-500/50 border px-5 py-3 rounded-xl focus:outline-dotted focus:outline-1transition duration-300 ease-out hover:ease-in">Log
+                    In</a>
+                <a href="#" class=" px-5 py-3 rounded-xl hover:shadow-md shadow-lg shadow-blue-800  bg-blue-700 hover:bg-blue-700 active:bg-blue-900 text-white font-bold transition duration-300 ease-out hover:ease-in">Sing
+                    Up</a>
+            </div>
+
+        </div>
+
+    </div> 
+        </>
+    )
+}
+export default NavDesktop
